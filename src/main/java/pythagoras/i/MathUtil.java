26cf80f38f7812431d8abd0380594e3d03498ff5
Michael Bayne <mdb@samskivert.com>
2011-11-16 13:55:34 -0800

    Added an integer MathUtil with clamp(low, value, high).

---
+++ src/main/java/pythagoras/i/MathUtil.java
@@ -0,0 +1,20 @@
+//
+// Pythagoras - a collection of geometry classes
+// http://github.com/samskivert/pythagoras
+
+package pythagoras.i;
+
+/**
+ * Math utility methods.
+ */
+public class MathUtil
+{
+    /**
+     * Clamps the supplied {@code value} to between {@code low} and {@code high} (both inclusive).
+     */
+    public static int clamp (int low, int value, int high) {
+        if (value < low) return low;
+        if (value > high) return high;
+        return value;
+    }
+}
